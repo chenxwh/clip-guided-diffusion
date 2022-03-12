80e75c52c10ea0fa3a63ce74eeff166b8348b9b9
andreasjansson <andreas@replicate.ai>
2022-03-11 16:25:01 -0800

    Reformat with black

--- predict.py
+++ predict.py
@@ -13,63 +13,96 @@ from torchvision import transforms
 from torchvision.transforms import functional as TF
 import cog
 import clip
-from guided_diffusion.script_util import create_model_and_diffusion, model_and_diffusion_defaults
+from guided_diffusion.script_util import (
+    create_model_and_diffusion,
+    model_and_diffusion_defaults,
+)
 
 
 class Predictor(cog.Predictor):
     def setup(self):
-        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model_config = model_and_diffusion_defaults()
-        self.model_config.update({
-            'attention_resolutions': '32, 16, 8',
-            'class_cond': False,
-            'diffusion_steps': 1000,
-            'rescale_timesteps': True,
-            'timestep_respacing': '1000',  # Modify this value to decrease the number of timesteps.
-            'image_size': 256,
-            'learn_sigma': True,
-            'noise_schedule': 'linear',
-            'num_channels': 256,
-            'num_head_channels': 64,
-            'num_res_blocks': 2,
-            'resblock_updown': True,
-            'use_fp16': True,
-            'use_scale_shift_norm': True,
-        })
-        self.clip_model = clip.load('ViT-B/16', jit=False)[0].eval().requires_grad_(False).to(self.device)
+        self.model_config.update(
+            {
+                "attention_resolutions": "32, 16, 8",
+                "class_cond": False,
+                "diffusion_steps": 1000,
+                "rescale_timesteps": True,
+                "timestep_respacing": "1000",  # Modify this value to decrease the number of timesteps.
+                "image_size": 256,
+                "learn_sigma": True,
+                "noise_schedule": "linear",
+                "num_channels": 256,
+                "num_head_channels": 64,
+                "num_res_blocks": 2,
+                "resblock_updown": True,
+                "use_fp16": True,
+                "use_scale_shift_norm": True,
+            }
+        )
+        self.clip_model = (
+            clip.load("ViT-B/16", jit=False)[0]
+            .eval()
+            .requires_grad_(False)
+            .to(self.device)
+        )
         self.clip_size = self.clip_model.visual.input_resolution
 
-    @cog.input("prompt", default='alien friend by Odilon Redon', type=str, help="Text prompt")
+    @cog.input(
+        "prompt", default="alien friend by Odilon Redon", type=str, help="Text prompt"
+    )
     @cog.input("timesteps", type=int, help="Number of timesteps", default=1000)
     @cog.input("display_frequency", type=int, help="display frequency", default=10)
-    def predict(self, prompt='alien friend by Odilon Redon', timesteps=1000, display_frequency=10):
-        assert isinstance(timesteps, int) and timesteps > 0, 'timesteps is a positive integer'
-        assert isinstance(display_frequency, int) and display_frequency > 0, 'display_frequency is a positive integer'
+    def predict(
+        self,
+        prompt="alien friend by Odilon Redon",
+        timesteps=1000,
+        display_frequency=10,
+    ):
+        assert (
+            isinstance(timesteps, int) and timesteps > 0
+        ), "timesteps is a positive integer"
+        assert (
+            isinstance(display_frequency, int) and display_frequency > 0
+        ), "display_frequency is a positive integer"
         clip_model = self.clip_model
-        self.model_config.update({'timestep_respacing': str(timesteps)})
+        self.model_config.update({"timestep_respacing": str(timesteps)})
 
         # Load models
         model, diffusion = create_model_and_diffusion(**self.model_config)
-        model.load_state_dict(torch.load('256x256_diffusion_uncond.pt', map_location='cpu'))
+        model.load_state_dict(
+            torch.load("256x256_diffusion_uncond.pt", map_location="cpu")
+        )
         model.requires_grad_(False).eval().to(self.device)
-        if self.model_config['use_fp16']:
+        if self.model_config["use_fp16"]:
             model.convert_to_fp16()
 
-        normalize = transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073],
-                                         std=[0.26862954, 0.26130258, 0.27577711])
-        lpips_model = lpips.LPIPS(net='vgg').to(self.device)
+        normalize = transforms.Normalize(
+            mean=[0.48145466, 0.4578275, 0.40821073],
+            std=[0.26862954, 0.26130258, 0.27577711],
+        )
+        lpips_model = lpips.LPIPS(net="vgg").to(self.device)
 
         prompts = [prompt]
         image_prompts = []
         batch_size = 1
-        clip_guidance_scale = 1000  # Controls how much the image should look like the prompt.
+        clip_guidance_scale = (
+            1000  # Controls how much the image should look like the prompt.
+        )
         tv_scale = 150  # Controls the smoothness of the final output.
         range_scale = 50  # Controls how far out of range RGB values are allowed to be.
         cutn = 16
-        init_image = None  # This can be an URL or Colab local path and must be in quotes.
-        skip_timesteps = 0  # This needs to be between approx. 200 and 500 when using an init image.
+        init_image = (
+            None  # This can be an URL or Colab local path and must be in quotes.
+        )
+        skip_timesteps = (
+            0  # This needs to be between approx. 200 and 500 when using an init image.
+        )
         # Higher values make the output look more like the init.
-        init_scale = 0  # This enhances the effect of the init image, a good value is 1000.
+        init_scale = (
+            0  # This enhances the effect of the init image, a good value is 1000.
+        )
         seed = 0
 
         gc.collect()
@@ -78,19 +111,25 @@ class Predictor(cog.Predictor):
             torch.manual_seed(seed)
 
         make_cutouts = MakeCutouts(self.clip_size, cutn)
-        side_x = side_y = self.model_config['image_size']
+        side_x = side_y = self.model_config["image_size"]
 
         target_embeds, weights = [], []
 
         for prompt in prompts:
             txt, weight = parse_prompt(prompt)
-            target_embeds.append(clip_model.encode_text(clip.tokenize(txt).to(self.device)).float())
+            target_embeds.append(
+                clip_model.encode_text(clip.tokenize(txt).to(self.device)).float()
+            )
             weights.append(weight)
 
         for prompt in image_prompts:
             path, weight = parse_prompt(prompt)
-            img = Image.open(fetch(path)).convert('RGB')
-            img = TF.resize(img, min(side_x, side_y, *img.size), transforms.InterpolationMode.LANCZOS)
+            img = Image.open(fetch(path)).convert("RGB")
+            img = TF.resize(
+                img,
+                min(side_x, side_y, *img.size),
+                transforms.InterpolationMode.LANCZOS,
+            )
             batch = make_cutouts(TF.to_tensor(img).unsqueeze(0).to(self.device))
             embed = clip_model.encode_image(normalize(batch)).float()
             target_embeds.append(embed)
@@ -99,12 +138,12 @@ class Predictor(cog.Predictor):
         target_embeds = torch.cat(target_embeds)
         weights = torch.tensor(weights, device=self.device)
         if weights.sum().abs() < 1e-3:
-            raise RuntimeError('The weights must not sum to 0.')
+            raise RuntimeError("The weights must not sum to 0.")
         weights /= weights.sum().abs()
 
         init = None
         if init_image is not None:
-            init = Image.open(fetch(init_image)).convert('RGB')
+            init = Image.open(fetch(init_image)).convert("RGB")
             init = init.resize((side_x, side_y), Image.LANCZOS)
             init = TF.to_tensor(init).to(self.device).unsqueeze(0).mul(2).sub(1)
 
@@ -113,21 +152,27 @@ class Predictor(cog.Predictor):
         def cond_fn(x, t, out, y=None):
             n = x.shape[0]
             fac = diffusion.sqrt_one_minus_alphas_cumprod[cur_t]
-            x_in = out['pred_xstart'] * fac + x * (1 - fac)
+            x_in = out["pred_xstart"] * fac + x * (1 - fac)
             clip_in = normalize(make_cutouts(x_in.add(1).div(2)))
             image_embeds = clip_model.encode_image(clip_in).float()
-            dists = spherical_dist_loss(image_embeds.unsqueeze(1), target_embeds.unsqueeze(0))
+            dists = spherical_dist_loss(
+                image_embeds.unsqueeze(1), target_embeds.unsqueeze(0)
+            )
             dists = dists.view([cutn, n, -1])
             losses = dists.mul(weights).sum(2).mean(0)
             tv_losses = tv_loss(x_in)
-            range_losses = range_loss(out['pred_xstart'])
-            loss = losses.sum() * clip_guidance_scale + tv_losses.sum() * tv_scale + range_losses.sum() * range_scale
+            range_losses = range_loss(out["pred_xstart"])
+            loss = (
+                losses.sum() * clip_guidance_scale
+                + tv_losses.sum() * tv_scale
+                + range_losses.sum() * range_scale
+            )
             if init is not None and init_scale:
                 init_losses = lpips_model(x_in, init)
                 loss = loss + init_losses.sum() * init_scale
             return -torch.autograd.grad(loss, x)[0]
 
-        if self.model_config['timestep_respacing'].startswith('ddim'):
+        if self.model_config["timestep_respacing"].startswith("ddim"):
             sample_fn = diffusion.ddim_sample_loop_progressive
         else:
             sample_fn = diffusion.p_sample_loop_progressive
@@ -152,7 +197,7 @@ class Predictor(cog.Predictor):
         for j, sample in enumerate(samples):
             cur_t -= 1
             if j % display_frequency == 0 or cur_t == -1:
-                for k, image in enumerate(sample['pred_xstart']):
+                for k, image in enumerate(sample["pred_xstart"]):
                     yield checkin(j, k, image, out_path)
 
         return out_path
@@ -161,35 +206,37 @@ class Predictor(cog.Predictor):
 @torch.no_grad()
 def checkin(j, k, image, out_path):
     if j is not None:
-        sys.stderr.write('\n')
-        sys.stderr.write(f' ****Progressive output, step: {j}, output: {k}***\n')
+        sys.stderr.write("\n")
+        sys.stderr.write(f" ****Progressive output, step: {j}, output: {k}***\n")
     TF.to_pil_image(image.add(1).div(2).clamp(0, 1)).save(str(out_path))
     return out_path
 
 
 def fetch(url_or_path):
-    if str(url_or_path).startswith('http://') or str(url_or_path).startswith('https://'):
+    if str(url_or_path).startswith("http://") or str(url_or_path).startswith(
+        "https://"
+    ):
         r = requests.get(url_or_path)
         r.raise_for_status()
         fd = io.BytesIO()
         fd.write(r.content)
         fd.seek(0)
         return fd
-    return open(url_or_path, 'rb')
+    return open(url_or_path, "rb")
 
 
 def parse_prompt(prompt):
-    if prompt.startswith('http://') or prompt.startswith('https://'):
-        vals = prompt.rsplit(':', 2)
-        vals = [vals[0] + ':' + vals[1], *vals[2:]]
+    if prompt.startswith("http://") or prompt.startswith("https://"):
+        vals = prompt.rsplit(":", 2)
+        vals = [vals[0] + ":" + vals[1], *vals[2:]]
     else:
-        vals = prompt.rsplit(':', 1)
-    vals = vals + ['', '1'][len(vals):]
+        vals = prompt.rsplit(":", 1)
+    vals = vals + ["", "1"][len(vals) :]
     return vals[0], float(vals[1])
 
 
 class MakeCutouts(nn.Module):
-    def __init__(self, cut_size, cutn, cut_pow=1.):
+    def __init__(self, cut_size, cutn, cut_pow=1.0):
         super().__init__()
         self.cut_size = cut_size
         self.cutn = cutn
@@ -201,10 +248,12 @@ class MakeCutouts(nn.Module):
         min_size = min(sideX, sideY, self.cut_size)
         cutouts = []
         for _ in range(self.cutn):
-            size = int(torch.rand([])**self.cut_pow * (max_size - min_size) + min_size)
+            size = int(
+                torch.rand([]) ** self.cut_pow * (max_size - min_size) + min_size
+            )
             offsetx = torch.randint(0, sideX - size + 1, ())
             offsety = torch.randint(0, sideY - size + 1, ())
-            cutout = input[:, :, offsety:offsety + size, offsetx:offsetx + size]
+            cutout = input[:, :, offsety : offsety + size, offsetx : offsetx + size]
             cutouts.append(F.adaptive_avg_pool2d(cutout, self.cut_size))
         return torch.cat(cutouts)
 
@@ -217,10 +266,10 @@ def spherical_dist_loss(x, y):
 
 def tv_loss(input):
     """L2 total variation loss, as in Mahendran et al."""
-    input = F.pad(input, (0, 1, 0, 1), 'replicate')
+    input = F.pad(input, (0, 1, 0, 1), "replicate")
     x_diff = input[..., :-1, 1:] - input[..., :-1, :-1]
     y_diff = input[..., 1:, :-1] - input[..., :-1, :-1]
-    return (x_diff**2 + y_diff**2).mean([1, 2, 3])
+    return (x_diff ** 2 + y_diff ** 2).mean([1, 2, 3])
 
 
 def range_loss(input):
